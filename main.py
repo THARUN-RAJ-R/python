256ef31954e9c044503a8ab6dce8b993865c9520
THARUN RAJ R <tharunrajr2004@gmail.com>
2024-06-26 17:24:23 +0530

    delete

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 from fastapi import FastAPI
 from typing import  Optional
 from pydantic import BaseModel
-student={1:{'name':'john','age':20,'year':2}}
+
+student={1:{'name':'john','age':20,'year':2},2:{'name':'ram','age':25,'year':3}}
 
 app = FastAPI()
 
 
 @app.get("/")
 def index():
-    return "hi" 
+    return "details" 
 
-@app.get("/about")
-def about():
-    return {"data": "about page"}
-
+#get method
 # {id} is path parameter
-@app.get("/about/{id}")
+@app.get("/show/{id}")
 #limit and l are query parameter
-#doubt what is use of optional
-def dec(id:int,limit:int=None,l:Optional[int]=76):
-    return {"data": id,"limit":limit,"l":l}
+def show(id:int,limit:int=20):
+    return f'{student[id]['name']} has limit ={limit}'
 
 #request method
 class req(BaseModel):
@@ -33,3 +30,25 @@ def user(stdid,stud:req):
     return student
 
 
+#put method
+class updatestd(BaseModel):
+    name:Optional[str]=None
+    age:Optional[int]=None
+    year:Optional[int]=None
+
+@app.put("/show/{stdid}")
+def update(stdid:int,updat:updatestd):
+    if updat.name is not None:
+        student[stdid]['name'] = updat.name
+    if updat.age is not None:
+        student[stdid]['age'] = updat.age
+    if updat.year is not None:
+        student[stdid]['year'] = updat.year
+    return student
+
+
+#delete method
+@app.delete("/show/{stdid}")
+def delete(stdid:int):
+    del student[stdid]
+    return student
