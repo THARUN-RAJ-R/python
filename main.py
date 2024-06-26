36622af90fecca84d10631017547d5da8b2245ae
THARUN RAJ R <tharunrajr2004@gmail.com>
2024-06-26 13:23:07 +0530

    post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import  Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -11,6 +12,19 @@ def index():
 def about():
     return {"data": "about page"}
 
+# {id} is path parameter
 @app.get("/about/{id}")
-def dec(id:int,limit:int=10,l:int=20,):
+#limit and l are query parameter
+#doubt what is use of optional
+def dec(id:int,limit:int=None,l:Optional[int]=76):
     return {"data": id,"limit":limit,"l":l}
+
+#request body
+class req(BaseModel):
+    name:str
+    age:int
+    phone:Optional[int]
+
+@app.post("/create")
+def create(blog:req):
+    return blog
