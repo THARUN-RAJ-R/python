ca2b0ee7b8f8f8edef1afd5101541c7aabf69a9e
THARUN RAJ R <tharunrajr2004@gmail.com>
2024-06-28 10:36:06 +0530

    python

--- string_1.py
+++ string_1.py
@@ -3,5 +3,8 @@ print(a[1:4])
 print(a[-4:-1])
 a = "Hello, World!"
 b = list(a)
+print(a.isupper())
+print(a.replace("World","python"))
 print(b)
 print(a[2])
+print(a.find("l"))
--- tuple.py
+++ tuple.py
@@ -1,7 +1,7 @@
 a=('1','2','3','4','3')
 b=('5','6','7','8')
 print(a[3])
-print(a.count())
+print(a.count(3))
 print(b[:4])
 c=a+b
-print(c)
+print(c.index('3'))
